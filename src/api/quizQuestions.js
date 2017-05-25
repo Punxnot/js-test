35fa69332bb318dde1f91be33f6ba0328a0c93ba
Marina <do-you-speak@yandex.ru>
2017-05-26 01:53:37 +0300

    Initial commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "What dish do you prefer?",
+      answers: [
+          {
+              type: "Fish",
+              content: "Fish soup"
+          },
+          {
+              type: "Meat",
+              content: "Chili con carne"
+          },
+          {
+              type: "Vegetables",
+              content: "Salad"
+          }
+      ]
+  },
+  {
+      question: "Which do you like more?",
+      answers: [
+          {
+              type: "Fish",
+              content: "Sea"
+          },
+          {
+              type: "Meat",
+              content: "City"
+          },
+          {
+              type: "Vegetables",
+              content: "Forest"
+          }
+      ]
+  },
+  {
+      question: "What do you like to do?",
+      answers: [
+          {
+              type: "Fish",
+              content: "Swim"
+          },
+          {
+              type: "Meat",
+              content: "Lie"
+          },
+          {
+              type: "Vegetables",
+              content: "Run"
+          }
+      ]
+  },
+  {
+      question: "Which of these games do you think is best?",
+      answers: [
+          {
+              type: "Fish",
+              content: "SwimmingFish"
+          },
+          {
+              type: "Meat",
+              content: "Funny sausages"
+          },
+          {
+              type: "Vegetables",
+              content: "Evil cabbage"
+          }
+      ]
+  },
+  {
+      question: "What pet would you prefer to own?",
+      answers: [
+          {
+              type: "Fish",
+              content: "Goldfish"
+          },
+          {
+              type: "Meat",
+              content: "Pig"
+          },
+          {
+              type: "Vegetables",
+              content: "Flower"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
