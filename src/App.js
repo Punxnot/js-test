ab4df9776ac2e9ee86192a54614f6490cdfe9575
Marina <do-you-speak@yandex.ru>
2017-05-27 02:00:58 +0300

    Add to styles

--- src/App.js
+++ src/App.js
@@ -120,7 +120,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>JS Quiz</h2>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
